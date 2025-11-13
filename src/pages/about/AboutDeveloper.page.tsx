a88a6cda820b1a93b1af4368f829a8d0d059e98b
Tobio <revolverc24@gmail.com>
2025-11-13 15:22:30 +0900

    \feat: add more to about developer page

--- src/pages/about/AboutDeveloper.page.tsx
+++ src/pages/about/AboutDeveloper.page.tsx
@@ -6,7 +6,7 @@ function AboutDeveloper() {
     return (
         <>
             <PageSection>
-                <Typography variant="h4">
+                <Typography variant="h4" sx={{ paddingTop: '20px' }}>
                     Hello!
                 </Typography>
                 <Typography variant="body1">
@@ -25,6 +25,34 @@ function AboutDeveloper() {
                     </a>
                     , a utility I wrote for merging and splitting the tiles needed for
                     this application.
+                </Typography>
+                <Typography variant="h4" sx={{ paddingTop: '20px' }}>
+                    Career
+                </Typography>
+                <Typography variant="body1">
+                    As a Front End Engineer, I most recently worked for a company called Lunit that makes AI models that can diagnose cancer,
+                    and assess the effectiveness of medicine for cancer. <br />In the front end team, we made a viewer called Scope Viewer (All-In-One)
+                    that visualises the data the AI models produce!
+                </Typography>
+                <Typography>
+                    I was deeply involved in setting up a JSON-customisable sidebar for it, which allowed one viewer page to visualise data for various AI models,
+                    with toggles controlling a range of visualisation pipelines from that JSON file.
+                </Typography>
+                <Typography>
+                    I was also involved in writing some of the visualisation code, including implementing WebGL so we could render millions of cells to the screen
+                    without causing lag. The bit-masking technique that Mars Viewer uses is one I was working on while at Lunit, but it never made it into a full release.
+                </Typography>
+                <Typography variant="h4" sx={{ paddingTop: '20px' }}>
+                    Personal Interests
+                </Typography>
+                <Typography variant="body1">
+                    I like video games, enough to try making them every now and again.
+                </Typography>
+                <Typography>
+                    I also like bikes, Judo, and playing the guitar.
+                </Typography>
+                <Typography>
+                    Also, I quite like space {":)"}
                 </Typography>
             </PageSection>
         </>
