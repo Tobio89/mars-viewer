32739d455a61fbc4ce56990fc43f00666e05d2d0
Tobio89 <revolverc24@gmail.com>
2025-11-17 18:46:08 +0900

    feat: apply tailwind for error indicator

--- src/components/ui/ErrorIndicator.tsx
+++ src/components/ui/ErrorIndicator.tsx
@@ -1,27 +1,30 @@
-import { Box, Typography } from "@mui/material"
+import { Typography } from "@mui/material";
 
-const ErrorIndicator = ({ message = "Error! Something's gone wrong" }: { message?: string }) => {
-    return (
-        <Box
-            component="main"
-            sx={{
-                flexGrow: 1,
-                display: "flex",
-                flexDirection: "row",
-                height: "100%",
-                width: "100%",
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
-        >
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '24px' }}>
+const ErrorIndicator = ({
+  message = "Error! Something's gone wrong",
+}: {
+  message?: string;
+}) => {
+  return (
+    <main
+      // component="main"
+      // sx={{
+      //     flexGrow: 1,
+      //     display: "flex",
+      //     flexDirection: "row",
+      //     height: "100%",
+      //     width: "100%",
+      //     alignItems: 'center',
+      //     justifyContent: 'center'
+      // }}
+      className="flex flex-1 shrink-0 h-full w-full items-center justify-center"
+    >
+      <div className="flex flex-col items-center justify-center gap-6">
+        <img src="/sad-white.png" style={{ height: "120px" }} />
+        <Typography variant="h5">{message}</Typography>
+      </div>
+    </main>
+  );
+};
 
-                <img src="/sad-white.png" style={{ height: '120px' }} />
-                <Typography variant="h5">
-                    {message}
-                </Typography>
-            </Box>
-        </Box>)
-}
-
-export default ErrorIndicator
+export default ErrorIndicator;
