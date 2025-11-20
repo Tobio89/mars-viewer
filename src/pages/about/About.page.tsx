0bde7cb2d2c58a352782d5b8cfc5a4f4f1cba251
Tobio89 <revolverc24@gmail.com>
2025-11-20 13:33:42 +0900

    feat: add detailed title for about page

--- src/pages/about/About.page.tsx
+++ src/pages/about/About.page.tsx
@@ -1,4 +1,4 @@
-import { useRoutes } from "react-router";
+import { useLocation, useRoutes } from "react-router";
 
 import { PageContainer, PageSection } from "@ui/common";
 import PageLink from "@ui/PageLink";
@@ -27,13 +27,29 @@ const aboutRouter = [
   },
 ];
 
+const getTitleFromPath = (path: string) => {
+  switch (path) {
+    case "/about/viewer":
+      return ": The Viewer";
+    case "/about/mars":
+      return ": The Planet";
+    case "/about/developer":
+      return ": The Developer";
+    default:
+      return "";
+  }
+};
+
 function AboutPage() {
   const element = useRoutes(aboutRouter);
+  const { pathname } = useLocation();
+
+  const aboutTitle = getTitleFromPath(pathname);
 
   return (
     <PageContainer>
       <PageSection>
-        <h3 className="text-4xl">About</h3>
+        <h3 className="text-4xl capitalize">About{aboutTitle}</h3>
       </PageSection>
       <PageSection>
         <div className="flex w-full justify-between">
